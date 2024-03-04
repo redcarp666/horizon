295dff6e1c52b1dbaf8eb1cd41bc4e5ec1a8b421
redcarp <sushuaihao@163.com>
2024-03-04 14:07:31 +0800

    flyway

--- horizon-infrastructure/src/main/java/org/redcarp/horizon/common/base/constant/RedisConstants.java
+++ horizon-infrastructure/src/main/java/org/redcarp/horizon/common/base/constant/RedisConstants.java
@@ -2,8 +2,6 @@ package org.redcarp.horizon.common.base.constant;
 
 public class RedisConstants {
 
-    public static final String TOKEN_PREFIX = "token:";
-    public static final String PERMISSION_PREFIX = "permission:";
     public static final String MENU_TREE_PREFIX = "menu:tree:";
     public static final String MENU_TREE_GLOBAL_PREFIX = "menu:tree:global";
     public static final String DEPT_TREE_GLOBAL_PREFIX = "dept:tree:global";
