1cd6bf76d15e24211cc34dd01a9007de50caf51b
redcarp <sushuaihao@163.com>
2024-04-15 15:17:57 +0800

    支持@Valid注解自定义异常key

--- horizon-infrastructure/src/main/java/org/redcarp/horizon/infrastructure/handler/GlobalExceptionHandler.java
+++ horizon-infrastructure/src/main/java/org/redcarp/horizon/infrastructure/handler/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package org.redcarp.horizon.infrastructure.handler;
 
+import org.apache.commons.lang3.StringUtils;
 import org.redcarp.horizon.infrastructure.domain.Response;
 import org.redcarp.horizon.infrastructure.exception.HorizonBusinessException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -25,11 +29,27 @@ public class GlobalExceptionHandler {
 	private MessageSource messageSource;
 
 	@ExceptionHandler(value = HorizonBusinessException.class)
-	public Response<Integer> InspurBusinessExceptionHandle(HorizonBusinessException exception) {
+	public Response<Integer> horizonBusinessExceptionHandle(HorizonBusinessException exception) {
 		String messageSourceMessage = messageSource.getMessage(exception.getMessageKey(),
 		                                                       exception.getParams(),
 		                                                       "something wrong",
 		                                                       LocaleContextHolder.getLocale());
 		return Response.fail(Optional.ofNullable(exception.getCode()).orElse(FAIL).intValue(), messageSourceMessage);
+	}
+
+	@ExceptionHandler(value = MethodArgumentNotValidException.class)
+	public Response<Integer> methodArgumentNotValidExceptionHandle(MethodArgumentNotValidException exception) {
+		FieldError fieldError = exception.getFieldError();
+		if (fieldError != null) {
+			String defaultMessage = fieldError.getDefaultMessage();
+			if (StringUtils.isNotBlank(defaultMessage)) {
+				String messageSourceMessage = messageSource.getMessage(defaultMessage,
+				                                                       null,
+				                                                       exception.getMessage(),
+				                                                       LocaleContextHolder.getLocale());
+				return Response.fail(HttpStatus.BAD_REQUEST.value(), messageSourceMessage);
+			}
+		}
+		return Response.fail();
 	}
 }
