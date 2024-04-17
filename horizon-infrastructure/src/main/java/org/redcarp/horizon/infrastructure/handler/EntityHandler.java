26134724c69966eb17d4c5e4bb56057ed41b28f7
redcarp <sushuaihao@163.com>
2024-04-17 17:04:02 +0800

    fix

--- horizon-infrastructure/src/main/java/org/redcarp/horizon/infrastructure/handler/EntityHandler.java
+++ horizon-infrastructure/src/main/java/org/redcarp/horizon/infrastructure/handler/EntityHandler.java
@@ -4,12 +4,11 @@ import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import org.apache.ibatis.reflection.MetaObject;
 import org.redcarp.horizon.core.security.SecurityUserHolder;
 import org.redcarp.horizon.core.util.EmptyUtils;
+import org.redcarp.horizon.infrastructure.domain.HorizonBaseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
-import java.util.Objects;
-import java.util.function.Supplier;
 
 /**
  * 实体处理器
@@ -26,34 +25,33 @@ public class EntityHandler implements MetaObjectHandler {
 
 	@Override
 	public void insertFill(MetaObject metaObject) {
-		if (securityUserHolder != null) {
-			String userName = securityUserHolder.getCurrentUserName();
-			if (EmptyUtils.isNotEmpty(userName)) {
-				this.strictInsertFill(metaObject, "createBy", String.class, userName);
-				this.strictInsertFill(metaObject, "updateBy", String.class, userName);
+		Object originalObject = metaObject.getOriginalObject();
+		if (originalObject instanceof HorizonBaseEntity) {
+			HorizonBaseEntity horizonBaseEntity = (HorizonBaseEntity) originalObject;
+			if (securityUserHolder != null) {
+				String userName = securityUserHolder.getCurrentUserName();
+				if (EmptyUtils.isNotEmpty(userName)) {
+					horizonBaseEntity.setCreateBy(userName);
+					horizonBaseEntity.setUpdateBy(userName);
+				}
 			}
-			this.strictInsertFill(metaObject, "createTime", Date.class, new Date());
-			this.strictInsertFill(metaObject, "updateTime", Date.class, new Date());
+			horizonBaseEntity.setCreateTime(new Date());
+			horizonBaseEntity.setUpdateTime(new Date());
 		}
-	}
-
-	@Override
-	public MetaObjectHandler strictFillStrategy(MetaObject metaObject, String fieldName, Supplier<?> fieldVal) {
-		Object obj = fieldVal.get();
-		if (Objects.nonNull(obj)) {
-			metaObject.setValue(fieldName, obj);
-		}
-		return this;
 	}
 
 	@Override
 	public void updateFill(MetaObject metaObject) {
-		if (securityUserHolder != null) {
-			String userName = securityUserHolder.getCurrentUserName();
-			if (EmptyUtils.isNotEmpty(userName)) {
-				this.strictInsertFill(metaObject, "updateBy", String.class, userName);
+		Object originalObject = metaObject.getOriginalObject();
+		if (originalObject instanceof HorizonBaseEntity) {
+			HorizonBaseEntity horizonBaseEntity = (HorizonBaseEntity) originalObject;
+			if (securityUserHolder != null) {
+				String userName = securityUserHolder.getCurrentUserName();
+				if (EmptyUtils.isNotEmpty(userName)) {
+					horizonBaseEntity.setUpdateBy(userName);
+				}
 			}
-			this.strictInsertFill(metaObject, "updateTime", Date.class, new Date());
+			horizonBaseEntity.setUpdateTime(new Date());
 		}
 	}
 }
