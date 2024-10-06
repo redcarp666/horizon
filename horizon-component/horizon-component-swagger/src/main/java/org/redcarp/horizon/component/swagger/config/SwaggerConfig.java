7fbcad8d46875aa9af1f8d0ef3fe732f88f3a344
redcarp <sushuaihao@163.com>
2024-10-06 18:05:13 +0800

    swagger

--- horizon-component/horizon-component-swagger/src/main/java/org/redcarp/horizon/component/swagger/config/SwaggerConfig.java
+++ horizon-component/horizon-component-swagger/src/main/java/org/redcarp/horizon/component/swagger/config/SwaggerConfig.java
@@ -21,54 +21,38 @@ import java.util.List;
 @Configuration
 @EnableSwagger2
 @EnableWebMvc
-public class SwaggerConfig implements WebMvcConfigurer{
+public class SwaggerConfig implements WebMvcConfigurer {
 
-    @Bean
-    public Docket docket(){
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select().paths(PathSelectors.any())
-                .apis(RequestHandlerSelectors.basePackage("org.redcarp"))
-                .build()
-                .securitySchemes(securitySchemes()).securityContexts(securityContexts());
-    }
+	@Bean
+	public Docket docket() {
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select().paths(PathSelectors.any()).apis(
+				RequestHandlerSelectors.basePackage("org.redcarp")).build().securitySchemes(securitySchemes()).securityContexts(
+				securityContexts());
+	}
 
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		/** swagger-ui 地址 */
+		registry.addResourceHandler("/**").addResourceLocations("classpath:/META-INF/resources/");
+	}
 
-    private ApiInfo apiInfo(){
-        return new ApiInfoBuilder()
-                .title("redcarp horizon")
-                .description("redcarp horizon")
-                .version("1.0.0")
-                .contact(new Contact("redcarp","http://www.baidu.com",""))
-                .build();
-    }
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("redcarp horizon").description("redcarp horizon").version("1.0.0").contact(new Contact(
+				"redcarp",
+				"http://www.baidu.com",
+				"")).build();
+	}
 
-    private List<SecurityScheme> securitySchemes(){
-        List<SecurityScheme> securitySchemeList = new ArrayList<SecurityScheme>();
-        securitySchemeList.add(new ApiKey("token", "token", "header"));
-        return securitySchemeList;
-    }
+	private List<SecurityScheme> securitySchemes() {
+		List<SecurityScheme> securitySchemeList = new ArrayList<SecurityScheme>();
+		securitySchemeList.add(new ApiKey("token", "token", "header"));
+		return securitySchemeList;
+	}
 
-    private List<SecurityContext> securityContexts()
-    {
-        return Arrays.asList(SecurityContext.builder()
-                .securityReferences(Arrays.asList(
-                        new SecurityReference("token", new AuthorizationScope[]{})
-                )).build());
-    }
-
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry)
-    {
-        /** swagger-ui 地址 */
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/META-INF/resources/");
-    }
-
-
-
-
+	private List<SecurityContext> securityContexts() {
+		return Arrays.asList(SecurityContext.builder().securityReferences(Arrays.asList(new SecurityReference("token",
+		                                                                                                      new AuthorizationScope[]{}))).build());
+	}
 
 
 }
